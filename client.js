1e9747d7b40ee32215d720627839283635cf9754
Jonas Svensson <jonassvensson4@hotmail.com>
2020-01-26 00:22:41 +0100

    Update client.js

--- client.js
+++ client.js
@@ -1,19 +1,25 @@
+// Server callback
+function serverCallback( name, data, cb ) {
+    (async() => { 
+        emitNet(name, data);
+	
+        let promise = new Promise(( resolve ) => {
+            onNet(name, ( result ) => {
+                resolve(result);
+            });
+        });
+        
+        let result = await promise;
+        cb(result);
+    })();
+}
+
 // Register client events
-RegisterNetEvent('jsfour-core:session');
-RegisterNetEvent('jsfour-core:gamemaster');
 RegisterNetEvent('jsfour-core:toNUI');
 RegisterNetEvent('jsfour-core:error');
 
-// Gamemaster events
-on('jsfour-core:gamemaster', ( event ) => {
-    switch ( event ) {
-        case 'ragdoll':
-            SetPedToRagdoll(GetPlayerPed(-1), 1000, 1000, 0, 0, 0, 0);
-        break;
-    }
-});
-
-// Server errors - added this since some people forgets to checks their server consoles for errors
+// Server errors - added this since some people forgets to checks their server consoles for errors..
 on('jsfour-core:error', ( error ) => {
     console.error(`[jsfour-core] server error: ${ error }`);
 });
+
